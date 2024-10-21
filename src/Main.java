920c0871d95cc299a50c2d38e8f7cf6d7f299156
MK <newlink05@mail.ru>
2024-10-21 16:42:10 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13_676;
+        int milePrice = 20;
+        int miles = ticketPrice / milePrice;
+        System.out.println(miles);
+
+    }
+}
